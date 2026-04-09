0003edd2bf5ab674ce2a2e1c1422b405be25ace1
Rojge Jericho Villanueva <rojgevillanueva@gmail.com>
2026-04-10 01:10:32 +0800

    Initial commit

---
+++ villanueva-client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: Rojge Jericho Villanueva <br />
+          Email: rojgevillanueva@gmail.com <br />
+          Other Personal Info:
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
